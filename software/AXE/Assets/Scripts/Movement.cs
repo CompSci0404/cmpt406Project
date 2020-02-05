0f952c56d153078949cc82dee66026abf849b2e9
AJ <aj.nargang@gmail.com>
2020-02-05 16:02:07 -0600

    Add Movement to Player
    Add movement to player to test the doors are functioning.

---
+++ software/AXE/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float move_speed = 5f;
+
+    public Rigidbody2D r_body;
+
+    Vector2 movement;
+
+    // Update is called once per frame and gets users inputs
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // After the update frame gets the users input, fixed update will move the player.
+    private void FixedUpdate()
+    {
+        r_body.MovePosition(r_body.position + movement * move_speed * Time.fixedDeltaTime);
+    }
+}
