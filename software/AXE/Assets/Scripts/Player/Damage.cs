c1cd0c42ec86227363334a6260ce444acf28e230
AJ <aj.nargang@gmail.com>
2020-02-05 21:23:10 -0600

    Add Player Attack Damage for Types
    Add call to new AI method for damage using the players damage stat.

--- software/AXE/Assets/Scripts/Player/Damage.cs
+++ software/AXE/Assets/Scripts/Player/Damage.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Damage : MonoBehaviour
 {
     GameObject parent;
+    private float damage;
 
     // Start is called before the first frame update
     void Start()
     {
         parent = transform.parent.gameObject;
-        float damage = parent.GetComponent<PlayerStats>().GetDamage();
+        damage = parent.GetComponent<PlayerStats>().GetDamage();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -19,17 +20,19 @@ public class Damage : MonoBehaviour
         {
             // Weapon does damage
             Debug.Log("Base Enemy interaction");
-
+            collision.gameObject.GetComponent<AIClass>().Damage(damage);
         }
         else if (collision.gameObject.tag == "Type1Enemy" && parent.tag == "Type1")
         {
             // Weapon does damage
             Debug.Log("Type 1 interaction");
+            collision.gameObject.GetComponent<AIClass>().Damage(damage);
         }
         else if (collision.gameObject.tag == "Type2Enemy" && parent.tag == "Type2")
         {
             // Weapon does damage
             Debug.Log("Type 2 interaction");
+            collision.gameObject.GetComponent<AIClass>().Damage(damage);
         }
     }
 }
