c2293a7f1fae579793479bc084f171d6de310143
Trooper0404 <mtradke@sasktel.net>
2020-02-18 11:14:25 -0600

    finished range AI, still needs damage added on and testing on it.

--- software/AXE/Assets/Scripts/AI/AIClass.cs
+++ software/AXE/Assets/Scripts/AI/AIClass.cs
@@ -9,12 +9,18 @@ public abstract class AIClass : MonoBehaviour
     public float fov;
     public float health;
     public float atkDamage;
+    public float toCloseRng;    /*only enforced when this unit is retreating*/
+    public float VelocityOfRngAtck;
+    public float rngAttackCoolDown; 
 
     protected DecisionTree rootOfTree;
-    private float saveSpeed;
-    private GameObject ply; 
 
-    //---[[pre-setup calls THESE NEED TO BE CALLED BEFORE ANYTHING!]]---//
+    private float saveSpeed;
+    private GameObject ply;
+    private List<GameObject> rngPrefabs; /*like a library of all projectiles */
+    private float coolDown;
+
+    //---[[pre-setup calls]]---//
 
     public void setSaveSpeed()
     {
@@ -27,6 +33,33 @@ public abstract class AIClass : MonoBehaviour
         this.ply = GameObject.FindWithTag("Player"); 
 
     }
+
+    public void setcoolDown()
+    {
+
+        this.coolDown = 0; 
+    } 
+
+    public void buildRngPrefabs()
+    {
+        rngPrefabs = new List<GameObject>(); 
+
+        object[] prefabs;
+        int counter = 0; 
+
+        prefabs = Resources.LoadAll("rangeAttacks", typeof(GameObject)); 
+
+        while(counter < prefabs.Length)
+        {
+
+            GameObject newItem = (GameObject)prefabs[counter];
+
+            rngPrefabs.Add(newItem);
+
+            counter++; 
+        }
+       
+    } 
 
     // --- [[ Damage to AI: ]] ---//
 
@@ -42,6 +75,47 @@ public abstract class AIClass : MonoBehaviour
         Destroy(this.gameObject);
     }
 
+    //---[[range attack actions]]---//
+
+    public void rngAttackPly()
+    {
+
+        if (coolDown != 0)
+        {
+
+            this.coolDown -= Time.deltaTime;
+
+            if (this.coolDown <= 0)
+            {
+                this.coolDown = 0;
+            }
+
+
+        }
+        else if (coolDown == 0)
+        {
+
+            // direction that AI is currently facing is where we want to shoot our object!
+            Vector2 direction = (ply.transform.position - this.transform.position).normalized;
+
+            GameObject newProjectile = Instantiate(rngPrefabs[0], this.transform.position, Quaternion.identity);
+
+            float Angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            newProjectile.transform.rotation = Quaternion.AngleAxis(Angle, Vector3.forward);
+
+            Physics2D.IgnoreCollision(newProjectile.GetComponent<PolygonCollider2D>(), this.gameObject.GetComponent<PolygonCollider2D>(), true);
+
+            newProjectile.GetComponent<Rigidbody2D>().AddForce(direction * VelocityOfRngAtck);
+
+            coolDown = this.rngAttackCoolDown;
+
+            Destroy(newProjectile, 3.0f); 
+        }
+
+    }
+
+
     //---[[Movement Decisions]]---//
 
     public bool EnemySpotted()
@@ -56,6 +130,19 @@ public abstract class AIClass : MonoBehaviour
         }
     }
 
+    public bool toClose()
+    {
+        if(Vector2.Distance(this.transform.position, ply.transform.position) < toCloseRng)
+        {
+            return true;
+        }else
+        {
+
+            return false; 
+        }
+    }
+
+
     //---[[Movement Actions!]]---//
 
     public void MoveTowardsPly()
@@ -64,10 +151,21 @@ public abstract class AIClass : MonoBehaviour
         this.transform.position = Vector2.MoveTowards(this.transform.position, ply.transform.position, speed * Time.deltaTime);
     }
 
-    public void MoveAwayFromPly()
+    /*This is more like a teleport we should keep it.*/
+    public void Teleport()
     {
         speed = saveSpeed;
         this.transform.position = -(Vector2.MoveTowards(this.transform.position, ply.transform.position, speed * Time.deltaTime));
+
+    }
+
+    public void MoveAwayFromPly()
+    {
+        speed = saveSpeed;
+
+        Vector2 direction = this.gameObject.transform.position - ply.transform.position;
+
+        transform.Translate(direction.normalized * speed * Time.deltaTime); 
 
     }
 
--- software/AXE/Assets/Scripts/AI/PassiveRangedEnemy.cs
+++ software/AXE/Assets/Scripts/AI/PassiveRangedEnemy.cs
@@ -5,17 +5,53 @@ using UnityEngine;
 /**
  * Higher ranged enemy that moves away from the player to attack
  */
-public class PassiveRangedEnemy : MonoBehaviour
+public class PassiveRangedEnemy : AIClass
 {
+
     // Start is called before the first frame update
     void Start()
     {
+
+        this.setSaveSpeed();
+        this.FindPly();
+        this.setcoolDown();
+        this.buildRngPrefabs();
+
+
+        //---[[building decision tree:]]---//
+
+        DecisionTree enemySpotted = new DecisionTree();
+
+        enemySpotted.buildDecision(this.EnemySpotted);
+
+        DecisionTree tooClose = new DecisionTree();
+        tooClose.buildDecision(this.toClose);
+
+        DecisionTree runAway = new DecisionTree();
+        runAway.buildAction(this.MoveAwayFromPly);
+
+        DecisionTree attack = new DecisionTree();
+        attack.buildAction(this.rngAttackPly);
+
+        DecisionTree idleChoice = new DecisionTree();
+        idleChoice.buildAction(this.Idle);
+
+
+        enemySpotted.Right(tooClose);
+        enemySpotted.Left(idleChoice);
+        tooClose.Right(runAway);
+        tooClose.Left(attack);
+
+        this.rootOfTree = enemySpotted; 
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        this.rootOfTree.search(); 
         
     }
 }
