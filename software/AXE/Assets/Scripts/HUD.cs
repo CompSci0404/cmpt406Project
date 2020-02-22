74f49aac1d240064e4f17731fdf4c6157347de79
AJ <aj.nargang@gmail.com>
2020-02-21 23:40:09 -0600

    HUD Script
    Try and get the HUD working with hearts that will update to no avail.

---
+++ software/AXE/Assets/Scripts/HUD.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    private Stack<GameObject> hearts;
+    public GameObject heart;
+
+    private PlayerStats stats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stats = FindObjectOfType<PlayerStats>();
+        for (int i = 0; i < stats.GetHearts(); i++)
+            HeartOnHUD();
+    }
+
+    public void BuildHeartPrefabs()
+    {
+        hearts = new Stack<GameObject>();
+
+        object[] prefabs;
+        int counter = 0;
+
+        prefabs = Resources.LoadAll("Heart", typeof(GameObject));
+
+        while (counter < prefabs.Length)
+        {
+            GameObject newItem = (GameObject)prefabs[counter];
+
+            hearts.Push(newItem);
+
+            counter++;
+        }
+    }
+
+    void HeartOnHUD()
+    {
+        //GameObject newHeart = Instantiate(hearts.Pop(), this.transform.position, Quaternion.identity);
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void RemoveHUDHeart()
+    {
+        //hearts.Pop();
+    }
+}
