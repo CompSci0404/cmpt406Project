037890d9ef3701334b548e645bbc3f04c27f4a41
Lasrrr <larsonpaul@live.ca>
2020-02-24 06:22:06 -0600

    Created basic directional doors and script

---
+++ software/AXE/Assets/Scripts/Rooms/Door.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public enum Compass
+    {
+        North, East, South, West
+    };
+
+    public Compass direction;
+
+    private Door destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AssignPartner(Door door)
+    {
+        destination = door;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject player = collision.gameObject;
+        if (!player.CompareTag("Player"))
+        {
+            return;
+        }
+
+        if (null == destination)
+        {
+            // throw new MissingReferenceException("The collided door does not have a destination.");
+            Debug.LogError("This door has no destination!");
+            return;
+        }
+
+        switch(direction)
+        {
+            case Compass.North:
+                player.transform.position = destination.transform.position + Vector3.up;
+                break;
+            case Compass.East:
+                player.transform.position = destination.transform.position + Vector3.right;
+                break;
+            case Compass.South:
+                player.transform.position = destination.transform.position + Vector3.down;
+                break;
+            case Compass.West:
+                player.transform.position = destination.transform.position + Vector3.left;
+                break;
+            default:
+                Debug.LogError("Reached default in switch statement.");
+                break;
+        }
+
+    }
+}
