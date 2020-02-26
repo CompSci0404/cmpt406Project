639e8169fa478891e89eaabea95ca5a0fc79b0a3
AJ <aj.nargang@gmail.com>
2020-02-26 16:37:04 -0600

    Add Leons Life Counter to the Project
    In the project and into the scene.

---
+++ software/AXE/Assets/Art/livesHudFolder/LiveCounter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiveCounter : MonoBehaviour
+{
+    private PlayerStats stats;
+    public GameObject[] lives;
+    public int NewlivesNum;
+    // Start is called before the first frame update
+
+    public void UpdateLives(int NewTotal)
+    {
+        if(NewTotal > lives.Length)
+        {
+            //throw error
+        }
+        else if (lives.Length == NewTotal)
+        {
+            lives[NewTotal - 1].GetComponent<Renderer>().sortingOrder = 1;
+        }
+        else
+        {
+            lives[NewTotal].GetComponent<Renderer>().sortingOrder = -1;
+            lives[NewTotal - 1].GetComponent<Renderer>().sortingOrder = 1;
+        }
+    }
+    void Start()
+    {
+        stats = FindObjectOfType<PlayerStats>();
+        foreach (var life in lives)
+        {
+            life.GetComponent<Renderer>().sortingOrder = -1;
+        }
+        NewlivesNum = stats.GetLives();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateLives(stats.GetLives()+1);
+    }
+}
