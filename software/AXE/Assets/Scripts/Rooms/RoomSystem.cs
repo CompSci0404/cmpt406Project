43ffe93a96d3bf700832532e806ab06a77c4b45e
Lasrrr <larsonpaul@live.ca>
2020-02-25 18:18:05 -0600

    Added method RoomClear

--- software/AXE/Assets/Scripts/Rooms/RoomSystem.cs
+++ software/AXE/Assets/Scripts/Rooms/RoomSystem.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class RoomSystem : MonoBehaviour
 {
+    bool isClear = false;
+
+    List<Transform> doors;
+
     // Start is called before the first frame update
     void Start()
     {
+        doors = new List<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    void RoomClear()
+    {
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
