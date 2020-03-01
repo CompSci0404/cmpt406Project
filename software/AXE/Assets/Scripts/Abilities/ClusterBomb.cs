d43bdfb1328bf2d0e5a2c85fe73f25f520700527
AJ <aj.nargang@gmail.com>
2020-02-29 22:10:48 -0600

    Change Scene to Test and Add Cluster Bomb
    Change scene to test abilities.
    Add a particle effect that will work to create the cluster bomb.

---
+++ software/AXE/Assets/Scripts/Abilities/ClusterBomb.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A cluster of powerful micro explosives is released in a large radius around the character who switched out, damaging all enemies caught in the blast.
+/// </summary>
+public class ClusterBomb : MonoBehaviour
+{
+    public GameObject bomb;
+    float power = 5f;
+    float radius = 1.5f;
+    float damage = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (bomb == enabled)
+        {
+            Invoke("Detonate", 1);
+        }
+    }
+
+    void Detonate()
+    {
+        Vector2 explosionPosition = bomb.transform.position;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPosition, radius);
+
+        foreach(Collider2D hit in colliders)
+        {
+            AIClass ai = hit.GetComponent<AIClass>();
+            Rigidbody2D rBody = hit.GetComponent<Rigidbody2D>();
+            if(ai != null)
+            {
+                ai.Damage(damage);
+            }
+            if(rBody != null)
+            {
+                Vector2 force = new Vector2(1, 1);
+                rBody.AddForceAtPosition(force, explosionPosition);
+            }
+        }
+    }
+}
