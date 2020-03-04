5bf0d9bfe12861b998502513b240d1d3fed9280e
AJ <aj.nargang@gmail.com>
2020-03-04 14:38:21 -0600

    Pickup Script and Sprite for it.
    Pickup script will be attached to all items and abilities and will give the player feedback on how to get items and abilities.

---
+++ software/AXE/Assets/Scripts/PickMeUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class will create a pick up message when the player gets close to an object that can be picked up
+/// </summary>
+public class PickMeUp : MonoBehaviour
+{
+    [SerializeField]
+    GameObject PickupMessage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PickupMessage.transform.position = new Vector2(this.gameObject.transform.position.x, this.gameObject.transform.position.y + 1);
+        PickupMessage.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        PickupMessage.SetActive(false);
+    }
+}
