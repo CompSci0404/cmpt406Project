9ed862a13df5954702312b5993f67bb57de2aaaf
Lasrrr <larsonpaul@live.ca>
2020-03-04 13:41:38 -0600

    Started development on level generator

--- software/AXE/Assets/Scripts/PCG/FloorGenerator.cs
+++ software/AXE/Assets/Scripts/PCG/FloorGenerator.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class FloorGenerator : MonoBehaviour
 {
+    private Dictionary<Vector2Int, GameObject> rooms;
+    private Dictionary<Vector2Int, int> model;
+
+
+    [SerializeField]
+    List<GameObject> roomPrefabs;
+
+    private void Awake()
+    {
+        rooms = new Dictionary<Vector2Int, GameObject>();
+        model = new Dictionary<Vector2Int, int>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +28,35 @@ public class FloorGenerator : MonoBehaviour
     {
         
     }
+
+    private Dictionary<Vector2Int, int> GenerateModel()
+    {
+        Dictionary<Vector2Int, int> tempModel = new Dictionary<Vector2Int, int>();
+
+        // TODO: replace with variables
+        int width = 7;
+        int height = 7;
+
+        bool RoomGeneration()
+        {
+
+
+            return false;
+        }
+
+        return null;
+    }
+
+    private void LoadRooms()
+    {
+
+    }
+
+    private List<Vector2Int> GetNeighbours(Vector2Int room)
+    {
+        List<Vector2Int> neighbours = new List<Vector2Int>();
+
+        return neighbours;
+    }
+
 }
