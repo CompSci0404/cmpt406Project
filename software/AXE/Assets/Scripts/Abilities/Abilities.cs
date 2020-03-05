cc3985633d6b2cd47315fbd510965724eab6aed4
AJ <aj.nargang@gmail.com>
2020-03-04 20:47:07 -0600

    Get The Items Dropping Correctly
    Grab your position and place it beneath you.

--- software/AXE/Assets/Scripts/Abilities/Abilities.cs
+++ software/AXE/Assets/Scripts/Abilities/Abilities.cs
@@ -33,6 +33,8 @@ public class Abilities : MonoBehaviour
         swapAvailable = true;
 
         position = gameObject.transform;
+
+        rBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -70,7 +72,8 @@ public class Abilities : MonoBehaviour
                 }
                 else
                 {
-                    GameObject dropA = Instantiate(aAbility.transform.GetChild(0).gameObject, rBody.position, Quaternion.identity);
+                    GameObject dropA = Instantiate(aAbility.transform.GetChild(0).gameObject,
+                        rBody.position = new Vector2(rBody.position.x + 0.5f, rBody.position.y + -0.5f) ,Quaternion.identity);
                     dropA.name = aAbility.transform.GetChild(0).gameObject.name;
                     Destroy(aAbility.transform.GetChild(0).gameObject);
                     GameObject A = Instantiate(AbilitiesInRange[i].gameObject, aAbility.transform, false);
@@ -98,7 +101,9 @@ public class Abilities : MonoBehaviour
                 }
                 else
                 {
-                    GameObject dropSwap = Instantiate(swapAbility.transform.GetChild(0).gameObject, rBody.position, Quaternion.identity);
+                    rBody.position = new Vector2(rBody.position.x + 1, rBody.position.y + 1);
+                    GameObject dropSwap = Instantiate(swapAbility.transform.GetChild(0).gameObject, 
+                        rBody.position = new Vector2(rBody.position.x + 0.5f, rBody.position.y + -0.5f), Quaternion.identity);
                     dropSwap.name = swapAbility.transform.GetChild(0).gameObject.name;
                     Destroy(swapAbility.transform.GetChild(0).gameObject);
                     GameObject Swap = Instantiate(AbilitiesInRange[i].gameObject, swapAbility.transform, false);
