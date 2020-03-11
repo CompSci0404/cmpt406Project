9c3bc7208739b7b3f5b7512ba42b9adcab648637
AJ <aj.nargang@gmail.com>
2020-03-10 19:01:32 -0600

    Create a SlowDown Script for the Player Swap
    Set the time scale down to half and bring it back up.

---
+++ software/AXE/Assets/Scripts/Player/TimeSlowSwap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSlowSwap : MonoBehaviour
+{
+    public float duration = 1f;
+    [SerializeField]
+    GameObject screenDimmer;
+
+    public void SlowForSwap()
+    {
+        Debug.Log("IN SLOW SWAP");
+        screenDimmer.SetActive(true);
+        Time.timeScale = .5f;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        Invoke("Disable", duration);
+    }
+
+    private void Disable()
+    {
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        screenDimmer.SetActive(false);
+    }
+
+}
