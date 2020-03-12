b2d2410e26649fe3453ec3fa2b126f41702a1809
AJ <aj.nargang@gmail.com>
2020-03-12 12:47:48 -0600

    Scripts for Functionality
    Spawn rooms, spawn boss in the last room.

---
+++ software/AXE/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    // 1 --> need bottom door
+    // 2 --> need top door
+    // 3 --> need left door
+    // 4 --> need right door
+
+    private RoomTemplates templates;
+    private int rand;
+    public bool spawned = false;
+
+    public float waitTime = 4f;
+
+    private void Start()
+    {
+        Destroy(gameObject, waitTime);
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", .1f);
+    }
+
+    private void Spawn()
+    {
+        if (spawned == false)
+        {
+            if (openingDirection == 1)
+            {
+                // need bottom door 
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 2)
+            {
+                // need top door
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 3)
+            {
+                // need left door
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 4)
+            {
+                // need right door
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, Quaternion.identity);
+            }
+            spawned = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Spawnpoint"))
+        {
+            if (collision.GetComponent<RoomSpawner>().spawned == false && spawned == false)
+            {
+                // Spawn wall 
+                Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            spawned = true;
+        }
+    }
+}
