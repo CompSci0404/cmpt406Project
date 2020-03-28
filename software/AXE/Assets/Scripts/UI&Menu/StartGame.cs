f62691a16b97ad5b95dc2f892ce0c9d067fbdc0e
AJ <aj.nargang@gmail.com>
2020-03-28 13:28:03 -0600

    Create a Controller Screen
    Has all of the controller buttons that the player needs mapped out on a picture of an xbox controller.

---
+++ software/AXE/Assets/Scripts/UI&Menu/StartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("StartButton"))
+        {
+            StartScene();
+        }
+    }
+
+    // Start Scene
+    public void StartScene()
+    {
+        int rand = Random.Range(1, 4);
+        SceneManager.LoadScene(rand);
+        Time.timeScale = 1f;
+        PauseMenu.GameIsPaused = false;
+    }
+}
