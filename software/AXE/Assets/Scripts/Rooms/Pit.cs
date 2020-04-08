cb7c49c3828637bc12fd1087710250e733bd89f5
AJ <aj.nargang@gmail.com>
2020-04-08 13:42:39 -0600

    Change Pits
    Thor and Daugr are blocked while Valk and the Enemies can pass through. It does not like the projectiles though.

--- software/AXE/Assets/Scripts/Rooms/Pit.cs
+++ software/AXE/Assets/Scripts/Rooms/Pit.cs
@@ -14,51 +14,59 @@ public class Pit : MonoBehaviour
             swapMessage = GameObject.FindGameObjectsWithTag("Message")[1];
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            PlayerMovement player = collision.GetComponent<PlayerMovement>();
+            PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
             PlayerStats stats = player.GetComponentInChildren<PlayerStats>();
 
             if (stats.GetControllerNumber() == 1)
             {
-                // Make Thor die 
-                stats.DontMove();
-                FindObjectOfType<AudioManager>().PlaySound("FallScream");
-                // Switch to Valk to fly out
-                swapMessage.transform.position = new Vector2(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y + 1);
-                swapMessage.SetActive(true);
+                //// Make Thor die 
+                //stats.DontMove();
+                //FindObjectOfType<AudioManager>().PlaySound("FallScream");
+                //// Switch to Valk to fly out
+                //swapMessage.transform.position = new Vector2(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y + 1);
+                //swapMessage.SetActive(true);
             }
             else
             {
+                Physics2D.IgnoreCollision(this.gameObject.GetComponent<Collider2D>(), collision.gameObject.GetComponent<Collider2D>());
                 // Valk can fly
             }
         }
-        else if (collision.CompareTag("BaseEnemy"))
+        else if (collision.gameObject.CompareTag("BaseEnemy"))
         {
-            if(collision.GetComponent<EnemyAnim>().isDragur)
+            if(collision.gameObject.GetComponent<EnemyAnim>().isDragur)
             {
                 // floor enemies should avoid the pits at all costs
-                collision.GetComponent<PathFinding>().SetPitCollision(true, this.gameObject); 
+                //collision.gameObject.GetComponent<PathFinding>().SetPitCollision(true, this.gameObject); 
             }
             else
-            { 
+            {
                 // Enemy can fly
+                Physics2D.IgnoreCollision(this.gameObject.GetComponent<Collider2D>(), collision.gameObject.GetComponent<Collider2D>());
             }
         }
-        else
+        else if (collision.gameObject.CompareTag("Projectile") || collision.gameObject.CompareTag("Arrow"))
         {
-            // do nothing
+            Physics2D.IgnoreCollision(this.gameObject.GetComponent<Collider2D>(), collision.gameObject.GetComponent<Collider2D>());
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             PlayerStats stats = collision.GetComponentInChildren<PlayerStats>();
             swapMessage.SetActive(false);
+        }
+
+        else if (collision.gameObject.CompareTag("Projectile") || collision.gameObject.CompareTag("Arrow"))
+        {
+            Physics2D.IgnoreCollision(this.gameObject.GetComponent<Collider2D>(), collision.GetComponent<Collider2D>());
         }
     }
 }
